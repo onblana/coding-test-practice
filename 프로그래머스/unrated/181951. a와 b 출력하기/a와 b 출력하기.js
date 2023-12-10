d09b049fece378b762ffedf7eeae88566f97b9ee
boran <51261847+onblana@users.noreply.github.com>
2023-12-11 00:03:44 +0900

    [level unrated] Title: a와 b 출력하기, Time: 45.15 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.js
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -5,13 +5,5 @@ const rl = readline.createInterface({
 }).on('line', (string) => {
     const ab = string.split(' ');
     console.log(`a = ${ab[0]}\nb = ${ab[1]}`);
+    rl.close();
 });
-
-// =============================
-// let input = [];
-
-// rl.on('line', function (line) {
-//     input = line.split(' ');
-// }).on('close', function () {
-//     console.log(Number(input[0]) + Number(input[1]));
-// });
