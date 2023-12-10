1b9553262697c141632058688903a252e8996df4
boran <51261847+onblana@users.noreply.github.com>
2023-12-11 00:05:14 +0900

    [level unrated] Title: 문자열 반복해서 출력하기, Time: 49.24 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
@@ -5,10 +5,9 @@ const rl = readline.createInterface({
 }).on('line', (string) => {
     const arr = string.split(' ');
     let answer = '';
-    //--arr[1]; // string을 number로 변환
     while(arr[1] > 0) {
         answer = answer.concat(arr[0]);
-        --arr[1];
+        --arr[1]; // arr[1]은 사실 문자이지만, 자바스크립트의 자동형변환으로 숫자 연산됨
     }
     console.log(answer);
     rl.close();
