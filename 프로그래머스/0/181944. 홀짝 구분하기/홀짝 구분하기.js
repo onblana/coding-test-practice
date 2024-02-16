0a63bda42b3248fbe4bfc3634162ea00bc068711
boran <51261847+onblana@users.noreply.github.com>
2024-02-16 12:53:35 +0900

    [level 0] Title: 홀짝 구분하기, Time: 42.52 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', function (line) {
+    const n = Number(line);
+    if (n % 2 === 0) console.log(`${n} is even`);
+    if (n % 2 !== 0) console.log(`${n} is odd`);
+    rl.close();
+});
