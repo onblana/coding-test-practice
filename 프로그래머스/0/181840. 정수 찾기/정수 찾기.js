054e71d1a5aee2ef31197e8ea865f72c09ba124b
boran <51261847+onblana@users.noreply.github.com>
2024-02-16 17:30:59 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -1,10 +1,3 @@
 function solution(num_list, n) {
-    let answer = 0;
-    num_list.forEach((num) => {
-        if (num === n) answer = 1;
-    });
-    // for (let i = 0; i < num_list.length; ++i) {
-    //     if (num_list[i] === n) answer = 1;
-    // }
-    return answer;
+    return num_list.includes(n) ? 1 : 0;
 }
