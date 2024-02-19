818db92ee246b8cc77b2a53c8508af6f8d9f01d5
boran <51261847+onblana@users.noreply.github.com>
2024-02-19 12:33:54 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,7 +1,7 @@
 function solution(numbers, n) {
-    let answer = 0;
-    for (let i = 0; answer <= n; i++) {
-        answer += numbers[i];
+    let sum = 0;
+    for (let i = 0; sum <= n; i++) {
+        sum += numbers[i];
     }
-    return answer;
+    return sum;
 }
