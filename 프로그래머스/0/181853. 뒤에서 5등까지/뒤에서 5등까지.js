e00a40ec01a1f58c7f990901efdc16a4dda17448
boran <51261847+onblana@users.noreply.github.com>
2024-02-20 14:45:20 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -1,9 +1,9 @@
 function solution(num_list) {
-    const sorted = num_list.sort((a, b) => a - b);
     const arr = [];
+    num_list.sort((a, b) => a - b);
     
     for (let i = 0; i < 5; ++i) {
-        arr.push(sorted.shift());
+        arr.push(num_list.shift());
     }
     
     return arr;
