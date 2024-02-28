f44e058f8fd894310d7def11893c905aec9e4044
boran <51261847+onblana@users.noreply.github.com>
2024-02-28 15:41:34 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,8 +1,10 @@
 function solution(num_list) {
-    let odd = ''; // 홀수
-    let even = '';  // 짝수
+    let odd = ''; // odd는 홀수
+    let even = '';  // even은 짝수
+    
     for (num of num_list) {
         num % 2 !== 0 ? odd += num : even += num;
     }
+    
     return Number(odd) + Number(even);
 }
