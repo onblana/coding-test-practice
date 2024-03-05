fdea54936bd5076dc57fbcd0f5039dca045f7767
boran <51261847+onblana@users.noreply.github.com>
2024-03-05 12:37:30 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,5 +1,5 @@
 function solution(num_list) {
-    return num_list.length > 10
-        ? num_list.reduce((acc, cur) => acc + cur)
-        : num_list.reduce((acc, cur) => acc * cur);
+    return num_list.reduce(
+        (a, c) => num_list.length > 10 ? a + c : a * c
+    );
 }
