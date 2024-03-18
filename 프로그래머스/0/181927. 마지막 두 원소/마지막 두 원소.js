f09239c4c7444488dbe06ac981ca3d6297c88811
boran <51261847+onblana@users.noreply.github.com>
2024-03-18 18:00:23 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,11 +1,5 @@
 function solution(num_list) {
-    const copy = [...num_list];
-    const last1 = copy[copy.length - 1];
-    const last2 = copy[copy.length - 2];
-    if (last1 > last2) {
-        num_list.push(last1 - last2);
-        return num_list;
-    }
-    num_list.push(last1 * 2);
+    const [last2, last] = num_list.slice(-2);
+    last > last2 ? num_list.push(last - last2) : num_list.push(last * 2);
     return num_list;
 }
