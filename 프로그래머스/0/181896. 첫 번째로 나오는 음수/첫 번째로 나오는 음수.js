a74ecb0c9e503dae3e144b18be2e96afb78b2145
boran <51261847+onblana@users.noreply.github.com>
2024-04-05 16:46:23 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,5 +1,3 @@
 function solution(num_list) {
-    const found = num_list.findIndex(e => e < 0);
-    if (found === undefined) return -1;
-    return found;
+    return num_list.findIndex(e => e < 0);
 }
