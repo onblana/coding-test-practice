e97b8413d46bb5789989eddd98b1e83e1a8ec62f
boran <51261847+onblana@users.noreply.github.com>
2024-06-16 17:03:47 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.46 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,10 +1,13 @@
+const operations = {
+  w: n => n + 1,
+  s: n => n - 1,
+  d: n => n + 10,
+  a: n => n - 10,
+};
+
 function solution(n, control) {
-    const arr = control.split('');
-    arr.forEach(char => {
-        if (char === "w") n += 1;
-        if (char === "s") n -= 1;
-        if (char === "d") n += 10;
-        if (char === "a") n -= 10;
-    })
-    return n;
+  for (const char of control) {
+    n = operations[char](n);
+  }
+  return n;
 }
