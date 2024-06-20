48eb7a25b826f4a75d1d2710ca92692a9997f267
boran <51261847+onblana@users.noreply.github.com>
2024-06-20 13:43:21 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,3 @@
 function solution(my_string, overwrite_string, s) {
-    const front = my_string.slice(0, s);
-    const back = my_string.slice(s + overwrite_string.length);
-    return front.concat(overwrite_string).concat(back);
+    return my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length);
 }
