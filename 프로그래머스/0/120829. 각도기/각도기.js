3b1ede9c7162faac683b991b30ebfccc2a3f2368
boran <51261847+onblana@users.noreply.github.com>
2024-07-04 14:05:54 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,5 +1,5 @@
 const solution = angle =>
     angle < 90 ? 1
-    : angle === 90 ? 2
-    : angle > 90 && angle < 180 ? 3
-    : 4;
+        : angle === 90 ? 2
+        : angle < 180 ? 3
+        : 4;
