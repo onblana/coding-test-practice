49d5589e32804754328c0b7f23ecefc75e7429c7
boran <51261847+onblana@users.noreply.github.com>
2024-07-04 13:58:23 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,8 +1,1 @@
-function solution(numbers) {
-    const sum = numbers.reduce((a, c) => a + c);
-    // let sum = 0;
-    // for (num of numbers) {
-    //     sum += num;
-    // }
-    return sum / numbers.length;
-}
+const solution = numbers => numbers.reduce((a, c) => a + c) / numbers.length;
