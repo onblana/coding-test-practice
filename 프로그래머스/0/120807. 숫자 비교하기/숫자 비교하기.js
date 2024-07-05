245f3db899c660cd6b3344a12009b94d45560766
boran <51261847+onblana@users.noreply.github.com>
2024-07-05 17:51:46 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,1 +1,8 @@
-const solution = (num1, num2) => num1 === num2 ? 1 : -1;
+function solution(num1, num2) {
+   let answer;
+    if (num1 === num2)
+        answer = 1
+    else
+        answer = -1
+    return answer;
+}
