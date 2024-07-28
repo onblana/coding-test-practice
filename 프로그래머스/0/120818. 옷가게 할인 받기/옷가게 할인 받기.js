919a142757d2c20b5af93d7cfa0b75f804c893e8
boran <51261847+onblana@users.noreply.github.com>
2024-07-28 22:43:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,8 +1,7 @@
 function solution(price) {
     let discounted = 0;
-    if (price < 100000) discounted = price;
-    if (price >= 100000 && price < 300000) discounted = price * 0.95;
-    if (price >= 300000 && price < 500000) discounted = price * 0.9;
-    if (price >= 500000) discounted = price * 0.8;
-    return Math.floor(discounted);
+    if (price < 100000) return price;
+    if (price < 300000) return Math.floor(price * 0.95);
+    if (price < 500000) return Math.floor(price * 0.9);
+    return discounted = price * 0.8;
 }
