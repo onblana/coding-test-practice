a20678ad9f25f3bb209c143ca73e23cf5d416d00
boran <51261847+onblana@users.noreply.github.com>
2024-08-09 14:20:07 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,5 +1,1 @@
-function solution(my_string, letter) {
-    const arr = [...my_string];
-    const answer = arr.filter(char => char !== letter);
-    return answer.join('');
-}
+const solution = (my_string, letter) => my_string.split(letter).join('');
