43b32df145b779a3304f941a8f34e45729a3821b
boran <51261847+onblana@users.noreply.github.com>
2024-08-31 20:41:46 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,7 +1,7 @@
 function solution(my_string) {
     let answer = 0;
     for (const char of my_string) {
-        const x = parseInt(char);
+        const x = Number(char);
         if (!isNaN(x)) answer += x;
     }
     return answer;
