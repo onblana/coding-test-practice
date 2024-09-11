59613940820bb90e91bd762758d4d021d6b87339
boran <51261847+onblana@users.noreply.github.com>
2024-09-11 20:42:21 +0900

    [level 0] Title: 접미사 배열, Time: 0.32 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,9 +1,8 @@
 function solution(my_string) {
-    const arr = [...my_string];
     const answer = [];
     let str = '';
     for (let i = my_string.length - 1; i >= 0; --i) {
-        str = arr[i].concat(str);
+        str = my_string[i].concat(str);
         answer.unshift(str);
     }
     return answer.sort();
