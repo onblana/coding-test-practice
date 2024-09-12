5da2fa233e9cfb235674226a3e99e34a4fe347bf
boran <51261847+onblana@users.noreply.github.com>
2024-09-12 14:53:21 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -2,8 +2,7 @@ function solution(num_list) {
     let a = 0;
     let b = 0;
     for (let i = 0; i < num_list.length; ++i) {
-        if (i % 2 === 0) a += num_list[i];
-        if (i % 2 !== 0) b += num_list[i];
+        i % 2 === 0 ? a += num_list[i] : b += num_list[i];
     }
     return a >= b ? a : b;
 }
