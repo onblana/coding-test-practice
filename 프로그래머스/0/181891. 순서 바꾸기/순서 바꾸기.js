331c7ac5122ff301811309440af89c94a29bd8e3
boran <51261847+onblana@users.noreply.github.com>
2024-12-12 16:20:10 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,5 +1,1 @@
-function solution(num_list, n) {
-    const front = num_list.slice(n);
-    const back = num_list.slice(0, n);
-    return [...front, ...back]
-}
+const solution = (num_list, n) => [...num_list.slice(n), ...num_list.slice(0, n)];
