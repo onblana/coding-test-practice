2ab31cbff9e121f5b1bcceea9151ecd43bb8b9a9
bran <51261847+onblana@users.noreply.github.com>
2025-11-15 15:23:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 124.10 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,9 @@
 function solution(participant, completion) {
-    // // 효율성테스트 실패
-    // participant.forEach((name) => {
-    //     if (completion.includes(name)) {
-    //         completion.splice(completion.indexOf(name), 1);
-    //     } else answer = name;
-    // });
-    
     participant.sort();
     completion.sort();
-    for (let i = 0; i < participant.length; ++i) {
-        if (participant[i] !== completion[i])
-            return participant[i];
+    const iterate = participant.length
+    
+    for (let i = 0; i < iterate; ++i) {
+        if (participant[i] !== completion[i]) return participant[i];
     }
 }
